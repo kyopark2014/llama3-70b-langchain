4f6786f2bb0e646dd0d9fdc87c894bf4ade13bd6
ksdyb.park <ksdyb@amazon.com>
2024-04-28 21:37:35 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -230,23 +230,25 @@ from langchain_core.prompts.chat import (
     MessagesPlaceholder,
 )
 
+from langchain_core.output_parsers import JsonOutputParser
 def general_conversation(connectionId, requestId, chat, query):
     global time_for_inference, history_length, token_counter_history    
     time_for_inference = history_length = token_counter_history = 0
     
     prompt = PromptTemplate(
-    template="""
-    <|begin_of_text|>
-        <|start_header_id|>system<|end_header_id|>
+        template="""
+        <|begin_of_text|>
+            <|start_header_id|>system<|end_header_id|>
         
-        다음의 Human과 Assistant의 친근한 이전 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant의 이름은 서연입니다. 답변은 한국어로 하세요.<|eot_id|>
+            다음의 Human과 Assistant의 친근한 이전 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant의 이름은 서연입니다. 답변은 한국어로 하세요.<|eot_id|>
         
-        <|start_header_id|>user<|end_header_id|>
+            <|start_header_id|>user<|end_header_id|>
         
-        {text}<|eot_id|>
+            {text}<|eot_id|>
         
-        <|start_header_id|>assistant<|end_header_id|>\n\n"""
-
+            <|start_header_id|>assistant<|end_header_id|>\n\n""",
+            input_variables=["text"],
+    )
     
     chain = prompt | chat | JsonOutputParser()
         
@@ -258,7 +260,7 @@ def general_conversation(connectionId, requestId, chat, query):
         stream = chain.invoke(
             {
                 # "history": history,
-                "text": query,
+                "text": query,ㄴ
             }
         )
         print('stream: ', stream)
